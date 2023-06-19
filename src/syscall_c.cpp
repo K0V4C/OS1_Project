b6c99f2d6af14fd3f828382c2767b4d97716121d
Lazar Kovacevic <lazarkovacevic2002@gmail.com>
2023-06-19 09:58:59 +0200

    Small qol changes

--- src/syscall_c.cpp
+++ src/syscall_c.cpp
@@ -2,26 +2,28 @@
 #include "../h/bit_masks.hpp"
 
 inline void set_and_ecall(uint64 code) {
-    asm  volatile("mv a4, a3");
-    asm  volatile("mv a3, a2");
-    asm  volatile("mv a2, a1");
-    asm  volatile("mv a1, a0");
-    asm volatile ("mv a0, %[code]": : [code] "r"  (code));
-    asm volatile ("ecall");
+    asm volatile("mv a4, a3");
+    asm volatile("mv a3, a2");
+    asm volatile("mv a2, a1");
+    asm volatile("mv a1, a0");
+    asm volatile("mv a0, %[code]": : [code] "r"  (code));
+    asm volatile("ecall");
 }
+
+#define  SET_RET(ret) asm volatile("mv %[mem],  a0": [mem] "=r" (ret))
 
 void* mem_alloc (size_t size) {
     asm volatile ("mv a0, %[size_in_bytes]": : [size_in_bytes] "r"  (size));
     set_and_ecall(OP_CODES::c_allocate_memory);
     uint64 volatile ptr;
-    asm volatile("mv %[mem],  a0": [mem] "=r" (ptr));
+    SET_RET(ptr);    // asm volatile("mv %[mem],  a0": [mem] "=r" (ptr));
     return (void*)ptr;
 }
 
 int mem_free (void* adr) {
     set_and_ecall(OP_CODES::c_free_memory);
     uint64 volatile ret ;
-    asm volatile("mv %[mem],  a0": [mem] "=r" (ret));
+    SET_RET(ret); //asm volatile("mv %[mem],  a0": [mem] "=r" (ret));
     return (int)ret;
 }
 
